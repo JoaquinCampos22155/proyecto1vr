e0b48f65436889554aac1e8959160469c898d011
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-04-30 23:48:38 -0600

    coneccionessql

--- backend/backend/connection.js
+++ backend/backend/connection.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise'
 const pool = mysql.createPool({
     // host: 'localhost',
     host:'mysql',
-    port: 3306,
-    user: 'chat_user',
-    database: 'chat_db',
-    password: 'chat_password',
+    port: 33068,
+    user: 'root',
+    database: 'blog_db',
+    password: 'root_password',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
