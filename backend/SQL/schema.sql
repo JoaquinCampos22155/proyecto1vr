10523041b8cc083ed67b5e307f9ee08b4e1bd3f4
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-07 20:49:52 -0600

    cambios de mejora para deploy

--- backend/SQL/schema.sql
+++ backend/SQL/schema.sql
@@ -10,5 +10,5 @@ FLUSH PRIVILEGES;
 CREATE TABLE IF NOT EXISTS blog_posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
+    description TEXT NOT NULL
 );
--- backend/backend/src/index.js
+++ backend/backend/src/index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import { getAllPosts, createPost, createDatabase, getTotalPosts, deletePost } from './db.js';
 
 const app = express();
-const port = 3000;
+const port = 3855;
 // This line is necessary to parse the request body
 app.use(express.json());
 
